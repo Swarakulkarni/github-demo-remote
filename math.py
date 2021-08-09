155e7cd832a63835605eb7b711cce0528e875668
swaralikulkarni <kulswaralip@gmail.com>
2021-08-09 13:19:14 +0530

    Added fn headers

--- math.py
+++ math.py
@@ -1,23 +1,16 @@
 #Add implementation
 def add(x,y):
-	return x+y
+	pass
 
 #Subtract implementation
 def subtract(x,y):
-	return x-y   #on master branch
+	pass
 
 #Multiply implementation
 def multiply(x,y):
-	return x*y   #on Bug456 branch
+	pass
 
 #Divide implementation
 def divide(x,y):
-	if y==0:
-		return DIVIDE_BY_0_ERROR
-	else
-		return x/y    #on master branch
-
-# Square implementation
-def square(x):
-	return x*x
+	pass
  
