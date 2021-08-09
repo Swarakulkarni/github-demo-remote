fc5434a505349031432993899e6b793824644035
SwaraliKulkarni <34955139+Swarakulkarni@users.noreply.github.com>
2021-08-09 13:12:53 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,23 @@
+#Add implementation
+def add(x,y):
+	return x+y
+
+#Subtract implementation
+def subtract(x,y):
+	return x-y   #on master branch
+
+#Multiply implementation
+def multiply(x,y):
+	return x*y   #on Bug456 branch
+
+#Divide implementation
+def divide(x,y):
+	if y==0:
+		return DIVIDE_BY_0_ERROR
+	else
+		return x/y    #on master branch
+
+# Square implementation
+def square(x):
+	return x*x
+ 
